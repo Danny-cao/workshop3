86f64bd27b4e30653df27f2815d7f836fc26c111
HUDannyCao <danny.cao@student.hu.nl>
2019-03-03 02:03:12 +0100

    restservices back-end commit

---
+++ src/main/java/workshop3/restservices/App.java
@@ -0,0 +1,13 @@
+package workshop3.restservices;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
